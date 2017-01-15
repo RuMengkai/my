16021045f59cdd1e440de2b7bba8fd01afee2558
茹孟凯 <874968552@qq.com>
2017-01-15 22:51:09 +0800

    init my page

---
+++ js/index.js
@@ -0,0 +1,69 @@
+//rem布局
+function initSize() {
+	var _h = document.documentElement.clientHeight||document.body.clientHeight;
+	var _size=_h/675*100;
+	document.documentElement.style.fontSize=_size+"px";
+}
+
+var _timer;
+var _n=0;//控制图片
+function bgloop() {
+	clearTimeout(_timer);
+	if(_n==5){
+		_n=0;
+	}
+	switch(_n){
+		case 0:$("#bg").animate({opacity:'0'},500,function () {
+				$("#bg").attr("src","images/pub_159.jpg");
+				$("#bg").animate({opacity:'1'},500);
+				_n++;
+			});break;
+		case 1:$("#bg").animate({opacity:'0'},500,function () {
+				$("#bg").attr("src","images/pub_288.jpg");
+				$("#bg").animate({opacity:'1'},500);
+				_n++;
+			});break;
+		case 2:$("#bg").animate({opacity:'0'},500,function () {
+				$("#bg").attr("src","images/pub_350.jpg");
+				$("#bg").animate({opacity:'1'},500);
+				_n++;
+			});break;
+		case 3:$("#bg").animate({opacity:'0'},500,function () {
+				$("#bg").attr("src","images/pub_43.jpg");
+				$("#bg").animate({opacity:'1'},500);
+				_n++;
+			});break;
+		case 4:$("#bg").animate({opacity:'0'},500,function () {
+				$("#bg").attr("src","images/bg-desktop.jpg");
+				$("#bg").animate({opacity:'1'},500);
+				_n++;
+			});break;
+	}
+	_timer=setTimeout( bgloop,5000);
+}
+function main(){
+	var _body=document.getElementsByTagName('html')[0];
+	var _bg=document.getElementById("bg");
+	_bg.style.height=_body.clientHeight+"px";
+	_body.style.overflow="hidden";
+}
+function over() {
+	$(".aboutme a").on("mouseover",function () {
+		$($(this).children("span")).stop();
+		$($(this).children("span")).animate({width: '130'},300);
+	});
+	$(".aboutme a").on("mouseout",function () {
+		$($(this).children("span")).stop();
+		$($(this).children("span")).animate({width: '0'},300);
+	});
+}
+$(function(){
+	initSize();
+	main();
+	over();
+	setTimeout(function() {
+		bgloop();
+	},5000)
+	
+})
+
